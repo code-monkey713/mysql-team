8c047d1ec2e06f2b53f9a04bc3641046daaf2524
Tony Tran <codemonkey713@gmail.com>
2021-02-25 00:17:20 -0600

    finished cleaning up the mySQLteamSchema and the tables created on mySQL server

--- mySQLteamSchema.sql
+++ mySQLteamSchema.sql
@@ -3,24 +3,35 @@ CREATE DATABASE mySQLteam_DB;
 
 USE mySQLteam_DB;
 
-CREATE TABLE employee(
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE manager (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT FOREIGN KEY,
-  manager_id INT FOREIGN KEY,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30)
-);
-
-CREATE TABLE role(
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT, 
   title VARCHAR(30), 
   salary DECIMAL, 
-  department_id INT FOREIGN KEY, 
-  PRIMARY KEY (id)
+  department_id INT,
+  PRIMARY KEY (id), 
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id), 
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES manager(id)
+); 
