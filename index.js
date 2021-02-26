fc1d7e6cf43123f6de4131dae9f71efc2e3c5428
Tony Tran <codemonkey713@gmail.com>
2021-02-25 22:49:06 -0600

    created successful connection and basic skeleton framework

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const questions = require('./questions.js');
-const buildREADMEmd = require('./template.js');
+// const buildREADMEmd = require('./template.js');
+const mysql = require('mysql2');
+require('dotenv').config();
 
-async function main() {
-  try {
-    const userData = await inquirer.prompt(questions);
-    let color;
-    switch(userData.license){
-      case 'MIT':
-      case 'ISC':
-        color = 'green'
-        break;
-      case 'Apache':
-      case 'GNU':
-        color = 'blue';
-        break;
-      default:
-        color = 'lightgrey';
-        break;
-    }
-    const readme = buildREADMEmd(userData, color);
-    fs.writeFileSync('./generated-README.md', readme);
-    console.log('File was successfully written.');
-  } catch (error) {
-    console.log(error);
-  }
-}
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-main();
+// async function main() {
+  
+  // try {
+  //   const userData = await inquirer.prompt(questions);
+  //   let color;
+  //   switch(userData.license){
+  //     case 'MIT':
+  //     case 'ISC':
+  //       color = 'green'
+  //       break;
+  //     case 'Apache':
+  //     case 'GNU':
+  //       color = 'blue';
+  //       break;
+  //     default:
+  //       color = 'lightgrey';
+  //       break;
+  //   }
+  //   // const readme = buildREADMEmd(userData, color);
+  //   // fs.writeFileSync('./generated-README.md', readme);
+  //   // console.log('File was successfully written.');
+  // } catch (error) {
+  //   console.log(error);
+  // }
+// }
+
+// main();
+
+const main = async () => {
+  
+  const { action } = await inquirer.prompt(questions);
+  console.log(action);
+  // if (action === 'Exit') return;
+
+  // if (action === 'Place a bid') placeABid();
+  // else if (action === 'Post a bid') postABid();
+};
+
+connection.connect((err) => {
+  if (err) throw (err);
+  console.log(`Connected on ${connection.threadId}`);
+  main();
+});
--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-const questions = [
+const actions = [
   {
     type: 'list',
     message: 'Choose a license for this project.',
@@ -10,41 +10,6 @@ const questions = [
     message: 'What is title of the project?',
     name: 'title',
   },
-  {
-    type: 'input',
-    message: 'Enter the description of your project!',
-    name: 'description',
-  },
-  {
-    type: 'input',
-    message: 'What are the installation instructions?',
-    name: 'installation',
-  },
-  {
-    type: 'input',
-    message: 'Define how to use the application.',
-    name: 'usage',
-  },
-  {
-    type: 'input',
-    message: 'Please provide contributing instructions for this project.',
-    name: 'contribution',
-  },
-  {
-    type: 'input',
-    message: 'What are the testing instructions?',
-    name: 'testing',
-  },
-  {
-    type: 'input',
-    message: 'What is your Github username?',
-    name: 'github',
-  },
-  {
-    type: 'input',
-    message: 'What is your email address?',
-    name: 'email',
-  },
 ];
 
-module.exports = questions;
+module.exports = actions;
